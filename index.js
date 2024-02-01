387743ddc62ff8c4fda30653b55000a67fa8de12
Shubham rakhecha <shubham.mentem@gmail.com>
2024-02-01 18:20:32 +0530

    changes done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/affiliate/:id", (req, res) => {
+    const id = req.params.id
+  return res
+    .header({
+      "Content-Type": "application/json",
+      "Set-Cookie": `afffilateId=${id}; Path=/; Secure; SameSite=None;`,
+      "access-control-allow-origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+    })
+    .send("maine beja");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
