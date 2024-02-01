c2602ccce81b0c8505e3023dfda26b73d156bb65
Shubham rakhecha <shubham.mentem@gmail.com>
2024-02-01 18:53:44 +0530

    changes doeng

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-const hostname = 'http://ec2-3-109-192-138.ap-south-1.compute.amazonaws.com/';
+const hostname = '3.109.192.138';
 app.get("/affiliate/:id", (req, res) => {
     const id = req.params.id
   return res
