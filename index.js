b3d7f2acc09287489f37826415ac7be732ad4091
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-02-01 21:11:53 +0530

    dponee

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 8000;
+const port = 80;
 
 app.get("/api/affiliate/:id", (req, res) => {
   const id = req.params.id
