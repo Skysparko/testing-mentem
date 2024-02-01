b6e57121e6728dcd1f3a0280ac7feef54b7a8e81
shubhamrakhecha5@gmail.com <shubhamrakhecha5@gmail.com>
2024-02-02 01:38:37 +0530

    some changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/api/affiliate/:id", (req, res) => {
     .header({
       "Content-Type": "application/json",
       "Set-Cookie": `afffilateId=${id}; Path=/; Secure; SameSite=None;`,
-      "access-control-allow-origin": "*",
+      "access-control-allow-origin": "https://65-2-142-47.sslip.io",
       "Access-Control-Allow-Credentials": "true",
     })
     .send("maine beja");
